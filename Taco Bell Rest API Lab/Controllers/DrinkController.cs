96fc8f1ea2528de60f8fe328da934900440173df
Maria Ragone <maria@ragone.net>
2023-08-16 17:02:30 -0400

    second commit

--- Taco Bell Rest API Lab/Controllers/DrinkController.cs
+++ Taco Bell Rest API Lab/Controllers/DrinkController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Taco_Bell_Rest_API_Lab.Models;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace Taco_Bell_Rest_API_Lab.Controllers
 {
@@ -9,18 +10,27 @@ namespace Taco_Bell_Rest_API_Lab.Controllers
     public class DrinkController : ControllerBase
     {
         private TacoBellDbContext _tacoBellDbContext = new TacoBellDbContext();
-
+        // /api/Drink
         [HttpGet]
         public List<Drink> GetAll()
         {
             return _tacoBellDbContext.Drinks.ToList();
         }
 
+        // /api/Drink/{Id}
         [HttpGet("{Id}")]
 
-        public Drink GetByID(int id)
+        public Drink GetByID(int Id)
         {
-            return _tacoBellDbContext.Drinks.FirstOrDefault(d => d.Id == id);
+            return _tacoBellDbContext.Drinks.FirstOrDefault(d => d.Id == Id);
+        }
+
+        [HttpPatch]
+        public Drink UpdateName(string oldName, string newName)
+        {
+            Drink d = Drinks.FirstOrDefault(d => d.Title.ToLower() == oldName.ToLower().Trim());
+            d.Name = newName;
+            return d;
         }
     }
 }
